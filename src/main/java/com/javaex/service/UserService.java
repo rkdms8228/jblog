a92f00b6dd66af298f353317908e05731a60caa2
가은 <rkdms8228@naver.com>
2022-07-07 18:09:07 +0900

    회원가입과 블로그 동시 생성

--- src/main/java/com/javaex/service/UserService.java
+++ src/main/java/com/javaex/service/UserService.java
@@ -3,7 +3,9 @@ package com.javaex.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.javaex.dao.BlogDao;
 import com.javaex.dao.UserDao;
+import com.javaex.vo.BlogVo;
 import com.javaex.vo.UserVo;
 
 @Service
@@ -12,6 +14,8 @@ public class UserService {
 	//필드
 	@Autowired
 	private UserDao userDao;
+	@Autowired
+	private BlogDao blogDao;
 
 	//생성자
 	
@@ -21,11 +25,21 @@ public class UserService {
 	
 	//메소드-일반
 	//회원가입 + 블로그 + 카테고리
-	public int join(UserVo userVo) {
+	public int join(UserVo userVo, BlogVo blogVo) {
 		
 		System.out.println("UserService > join");
 		
-		return userDao.join(userVo);
+		int count = userDao.join(userVo);
+		
+		blogVo.setId(userVo.getId());
+		blogVo.setBlogTitle(userVo.getUserName() + "의 블로그입니다.");
+		blogVo.setLogoFile("/assets/images/spring-logo.jpg");
+		
+		blogDao.make(blogVo);
+		
+		//categoryDao.**(userVo);
+		
+		return count;
 		
 	}
 	
