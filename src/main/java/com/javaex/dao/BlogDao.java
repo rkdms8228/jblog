6560cf9f99bf3ce4f6955aca363d721f00cb44ee
가은 <rkdms8228@naver.com>
2022-07-07 18:09:22 +0900

    회원가입과 블로그 동시 생성

---
+++ src/main/java/com/javaex/dao/BlogDao.java
@@ -0,0 +1,41 @@
+package com.javaex.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.BlogVo;
+
+@Repository
+public class BlogDao {
+	
+	//필드
+	@Autowired
+	private SqlSession sqlSession;
+	
+	//생성자
+	
+	
+	//메소드-gs
+	
+	
+	//메소드-일반
+	//블로그 생성
+	public int make(BlogVo blogVo) {
+		
+		System.out.println("BlogDao > make");
+		
+		return sqlSession.insert("blog.make", blogVo);
+		
+	}
+	
+	//블로그 메인화면
+	public int main(String id) {
+		
+		System.out.println("BlogDao > make");
+		
+		return sqlSession.selectOne("blog.main", id);
+		
+	}
+
+}
