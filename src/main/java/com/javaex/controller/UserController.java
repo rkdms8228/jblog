72ac888cce2c69534b4be2093b44be4b5a348d85
가은 <rkdms8228@naver.com>
2022-07-07 18:09:10 +0900

    회원가입과 블로그 동시 생성

--- src/main/java/com/javaex/controller/UserController.java
+++ src/main/java/com/javaex/controller/UserController.java
@@ -5,11 +5,13 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.javaex.service.UserService;
+import com.javaex.vo.BlogVo;
 import com.javaex.vo.UserVo;
 
 @Controller
@@ -39,11 +41,13 @@ public class UserController {
 	
 	//회원가입
 	@RequestMapping(value="/join", method={RequestMethod.GET, RequestMethod.POST})
-	public String join(@ModelAttribute UserVo userVo) {
+	public String join(@ModelAttribute UserVo userVo, @ModelAttribute BlogVo blogVo) {
 		
 		System.out.println("UserController > join");
 		
-		userService.join(userVo);
+		userService.join(userVo, blogVo);
+		
+		System.out.println(userVo);
 		
 		return "user/joinSuccess";
 		
@@ -84,7 +88,7 @@ public class UserController {
 		if(authUser != null) { //로그인 성공
 			
 			session.setAttribute("authUser", authUser);
-			return "redirect://";
+			return "redirect:/";
 			
 		}else { //로그인 실패
 			
@@ -93,6 +97,19 @@ public class UserController {
 			
 		}
 
+	}
+	
+	//로그아웃
+	@RequestMapping(value="/logout", method={RequestMethod.GET, RequestMethod.POST})
+	public String logout(@ModelAttribute UserVo userVo, HttpSession session) {
+		
+		System.out.println("UserController > logout");
+		
+		session.removeAttribute("authUser");
+		session.invalidate();
+		
+		return "redirect:/";
+		
 	}
 
 }
