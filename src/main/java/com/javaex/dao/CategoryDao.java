e8dccdcfd435f4a5a1c97810ed685b9315533cc8
가은 <rkdms8228@naver.com>
2022-07-10 22:07:33 +0900

    카테고리 리스트 추가 삭제

--- src/main/java/com/javaex/dao/CategoryDao.java
+++ src/main/java/com/javaex/dao/CategoryDao.java
@@ -1,8 +1,12 @@
 package com.javaex.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.CategoryVo;
 
 @Repository
 public class CategoryDao {
@@ -18,5 +22,40 @@ public class CategoryDao {
 	
 	
 	//메소드-일반
+	//카테고리 리스트
+	public List<CategoryVo> categoryList(String id) {
+		
+		System.out.println("CategoryDao > categoryList");
+		
+		return sqlSession.selectList("category.categoryList", id);
+		
+	}
+	
+	//카테고리 추가
+	public int categoryAdd(CategoryVo cateVo) {
+		
+		System.out.println("CategoryDao > categoryAdd");
+		
+		return sqlSession.insert("category.categoryAdd", cateVo);
+		
+	}
+
+	//카테고리 추가(1개 정보 가져오기)
+	public CategoryVo categoryOne(int cateNo) {
+		
+		System.out.println("CategoryDao > categoryOne");
+		
+		return sqlSession.selectOne("category.categoryOne", cateNo);
+		
+	}
+	
+	//카테고리 삭제
+	public int categoryDelete(CategoryVo cateVo) {
+		
+		System.out.println("CategoryDao > categoryDelete");
+		
+		return sqlSession.delete("category.categoryDelete", cateVo);
+		
+	}
 
 }
