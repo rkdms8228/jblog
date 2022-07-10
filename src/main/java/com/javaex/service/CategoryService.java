14a8432f8d646ae73e43b8c85ed814d49e70cf38
가은 <rkdms8228@naver.com>
2022-07-10 22:07:37 +0900

    카테고리 리스트 추가 삭제

--- src/main/java/com/javaex/service/CategoryService.java
+++ src/main/java/com/javaex/service/CategoryService.java
@@ -1,9 +1,12 @@
 package com.javaex.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.javaex.dao.CategoryDao;
+import com.javaex.vo.CategoryVo;
 
 @Service
 public class CategoryService {
@@ -19,5 +22,42 @@ public class CategoryService {
 	
 	
 	//메소드-일반
+	//카테고리 리스트
+	public List<CategoryVo> categoryList(String id) {
+		
+		System.out.println("CategoryService > categoryList");
+		
+		return categoryDao.categoryList(id);
+		
+	}
+	
+	//카테고리 추가
+	public CategoryVo categoryAdd(CategoryVo cateVo) {
+		
+		System.out.println("CategoryService > categoryAdd");
+		
+		categoryDao.categoryAdd(cateVo);
+		
+		return categoryDao.categoryOne(cateVo.getCateNo());
+	}
+
+	//카테고리 삭제
+	public String categoryDelete(CategoryVo cateVo) {
+		
+		System.out.println("CategoryService > categoryDelete");
+		
+		if(cateVo.getPostCnt() != 0) {	
+			return "fail";
+		}else {
+			int count = categoryDao.categoryDelete(cateVo);
+			
+			if(count < 0) {
+				return "fail";
+			}
+			
+			return "success";
+
+		}
+	}
 
 }
