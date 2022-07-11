f76f197edc8763501f9279e5d4e3fe211e50b13e
가은 <rkdms8228@naver.com>
2022-07-11 15:54:34 +0900

    메인 카테고리 리스트

--- src/main/java/com/javaex/service/PostService.java
+++ src/main/java/com/javaex/service/PostService.java
@@ -1,8 +1,13 @@
 package com.javaex.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.javaex.dao.BlogDao;
+import com.javaex.dao.CategoryDao;
 import com.javaex.dao.PostDao;
 import com.javaex.vo.PostVo;
 
@@ -10,6 +15,10 @@ import com.javaex.vo.PostVo;
 public class PostService {
 	
 	//필드
+	@Autowired
+	private BlogDao blogDao;
+	@Autowired
+	private CategoryDao categoryDao;
 	@Autowired
 	private PostDao postDao;
 	
@@ -20,6 +29,19 @@ public class PostService {
 	
 	
 	//메소드-일반
+	//리스트
+	public Map<String, Object> postList(String id) {
+		
+		System.out.println("PostService > postList");
+		
+		Map<String, Object> blogMap = new HashMap<String, Object>();
+		blogMap.put("blogVo", blogDao.main(id)); //헤더
+		blogMap.put("categoryList", categoryDao.postList(id)); //카테고리 리스트
+		
+		return blogMap;
+		
+	}
+	
 	//글쓰기
 	public int write(PostVo postVo) {
 		
